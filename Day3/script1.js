227e21a39d16a50916f623f9034813b402c65183
Shunmuga priya A <66465531+priya995@users.noreply.github.com>
2023-11-28 21:14:05 +0530

    Delete Day3 directory

--- Day3/script1.js
+++
@@ -1,8 +0,0 @@
-//qn:1
-let obj1={name:"person1",age:5};
-let obj2={age:5,name:"person1"};
-if(obj1==obj2)
-    console.log("objects are same");
-
-    else
-    console.log("objects are not same");
